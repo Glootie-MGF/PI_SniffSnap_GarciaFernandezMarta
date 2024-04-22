5da8fba11649dcf0e2ff244f7a6f6575277e54a6
MARTA GARCIA FERNANDEZ <6002693@viewnext.com>
2024-04-22 14:54:12 +0200

    probando ofi

--- app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/ApiService.kt
+++ app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/ApiService.kt
@@ -1,9 +1,5 @@
 package com.example.pi_sniffsnap_garciafernandezmarta.api
 
 import retrofit2.Retrofit
-<<<<<<<<< Temporary merge branch 1
-dsfdsfdsfdsfdsfdsf
-=========
 
->>>>>>>>> Temporary merge branch 2
 private val retrofit = Retrofit.Builder().baseUrl("https://todogs.herokuapp.com/api/v1/")
