05446661209b98ec25e816b79d254a68dbbf5169
Glootie-MGF <116271650+Glootie-MGF@users.noreply.github.com>
2024-04-22 14:38:24 +0200

    Update ApiService.kt

--- app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/ApiService.kt
+++ app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/ApiService.kt
@@ -1,5 +1,5 @@
 package com.example.pi_sniffsnap_garciafernandezmarta.api
 
 import retrofit2.Retrofit
-
+dsfdsfdsfdsfdsfdsf
 private val retrofit = Retrofit.Builder().baseUrl("https://todogs.herokuapp.com/api/v1/")
