4d3d3035516696685f8439f52438d28687ac4a63
MARTA GARCIA FERNANDEZ <6002693@viewnext.com>
2024-05-04 18:43:05 +0200

    Manejando excepción para usuario o contraseña incorrectos en login

--- app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/makeNetworkCall.kt
+++ app/src/main/java/com/example/pi_sniffsnap_garciafernandezmarta/api/makeNetworkCall.kt
@@ -1,18 +1,27 @@
 package com.example.pi_sniffsnap_garciafernandezmarta.api
 
 import com.example.pi_sniffsnap_garciafernandezmarta.R
-import com.example.pi_sniffsnap_garciafernandezmarta.api.dto.DogDTOMapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 import java.net.UnknownHostException
 
+private const val UNAUTHORIZED_ERR_CODE = 401
 suspend fun <T> makeNetworkCall(
     call: suspend () -> T
 ): ApiResponseStatus<T> {
     return withContext(Dispatchers.IO){
         try {
             ApiResponseStatus.Success(call())
+        } catch (e: UnknownHostException){
+            ApiResponseStatus.Error(R.string.error_unknown_host_exc)
+        } catch (e: retrofit2.HttpException){
+            val errorMessage = if (e.code() == UNAUTHORIZED_ERR_CODE){
+                R.string.wrong_user_or_password
+            } else {
+                R.string.unknown_error
+            }
+            ApiResponseStatus.Error(errorMessage)
         } catch (e: Exception){
             val errorMessage = when(e.message){
                 "sign_up_error" -> R.string.error_sign_up
@@ -21,8 +30,6 @@ suspend fun <T> makeNetworkCall(
                 else -> R.string.unknown_error
             }
             ApiResponseStatus.Error(errorMessage)
-        } catch (e: UnknownHostException){
-            ApiResponseStatus.Error(R.string.error_unknown_host_exc)
         }
     }
 }
